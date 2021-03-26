f5e0b404f25d4071eeee66cf86eca167d0410caa
Friederike Moeser <friederike.moeser@gmail.com>
2021-03-26 15:50:52 +0100

    Add filter function

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-import { fetchData } from "./lib/api.js";
+import { createCharacter } from "./lib/character.js";
 
 const fetchButton = document.querySelector(".header__search-button");
+
+fetchButton.addEventListener("click", () => {
+  clearCharacters();
+  createCharacter();
+});
 
 function clearCharacters() {
   const characterElements = document.querySelectorAll(".character");
   characterElements.forEach((section) => section.remove());
 }
-
-fetchButton.addEventListener("click", () => {
-  clearCharacters();
-  fetchData();
-});
